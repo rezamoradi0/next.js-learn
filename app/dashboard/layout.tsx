72a2ab84a33ddb3477ceb8883773c410a7840e75
rezamoradi0 <rezamoradi0933@gmail.com>
2025-07-04 21:24:25 +0330

    last commit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SideNav from "../ui/dashboard/sidenav";
 
+// export const exprimental_ppr = true;
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
@@ -9,5 +10,5 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             </div>
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
-      );
+    );
 }
